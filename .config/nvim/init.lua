0da9c967b67e3a01a53dff23c57183a26c999673
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2024-01-23 12:39:05 +0000

    clean up nvim lua files, install lazy/catppuccin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,25 @@
-require('core.keymaps')
-require('core.plugins')
-require('core.plugin_config')
+vim.opt.listchars = { tab = [[▸\ ]], eol = '¬', space = '.' }
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
