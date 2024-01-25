281e18590241beae206c9d8852a61ab10abb745f
Dovydas Lozys <755086+DovLozys@users.noreply.github.com>
2024-01-25 22:58:19 +0000

    adds treesitter with config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,7 +19,8 @@ local plugins = {
 	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.5',
 		dependencies = { 'nvim-lua/plenary.nvim' }
-	}
+	},
+	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
 }
 
 local opts = {}
@@ -29,6 +30,13 @@ require("lazy").setup(plugins, opts)
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+	ensure_installed = {"css", "go", "python", "javascript", "lua"},
+	highlight = { enable = true },
+        indent = { enable = true },
+})
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
